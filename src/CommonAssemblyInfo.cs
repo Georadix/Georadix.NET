47656779ddf3edd094f7a31be4871ecc5d722df8
Anthony <anthony@georadix.com>
2013-11-25 21:05:02 -0500

    Setup of the Core project.

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Georadix")]
+[assembly: AssemblyProduct("Georadix.NET")]
+[assembly: AssemblyCopyright("Copyright © 2013 Georadix")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
---
+++ src/Georadix.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Core Library")]
+[assembly: AssemblyDescription("Core Library")]
+
+[assembly: Guid("c706adfa-19c9-4bdf-adc4-fba5eb4d70f4")]
