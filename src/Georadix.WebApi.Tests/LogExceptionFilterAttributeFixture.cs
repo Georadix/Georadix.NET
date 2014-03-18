b99bc9c2df925bb3877a8eb6eb64fc1430fa75eb
Stephane Alie <stephane@georadix.com>
2014-03-18 18:16:01 -0400

    Added a Web API and Web API Testing project

---
+++ src/Georadix.WebApi.Tests/LogExceptionFilterAttributeFixture.cs
@@ -0,0 +1,92 @@
+﻿namespace Georadix.WebApi.Tests
+{
+    using Georadix.WebApi.Filters;
+    using Georadix.WebApi.Testing;
+    using SimpleInjector;
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+    using Xunit;
+
+    public class LogExceptionFilterAttributeFixture
+    {
+        [Fact]
+        public void CreateWithNullLoggerFactoryThrowsArgumentNullException()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => new LogExceptionFilterAttribute(null));
+
+            Assert.Equal("loggerFactory", ex.ParamName);
+        }
+
+        [Fact]
+        public async Task ExceptionFromControllerLoggedAsError()
+        {
+            using (var server = new InMemoryServer(new Container(), this.ConfigureServer))
+            {
+                var response = await server.Client.GetAsync("test/exception");
+
+                Assert.True(server.MockLoggers.ContainsKey(typeof(LogExceptionFilterAttributeFixtureController)));
+
+                server.MockLoggers[typeof(LogExceptionFilterAttributeFixtureController)].Verify(l => l.Error(
+                    LogExceptionFilterAttributeFixtureController.Exception.Message,
+                    LogExceptionFilterAttributeFixtureController.Exception));
+            }
+        }
+
+        [Fact]
+        public async Task ExceptionFromControllerLogsInnerException()
+        {
+            using (var server = new InMemoryServer(new Container(), this.ConfigureServer))
+            {
+                server.Container.Register<LogExceptionFilterAttributeFixtureController>();
+
+                var response = await server.Client.GetAsync("test/nestedxception");
+
+                Assert.True(server.MockLoggers.ContainsKey(typeof(LogExceptionFilterAttributeFixtureController)));
+
+                server.MockLoggers[typeof(LogExceptionFilterAttributeFixtureController)].Verify(l => l.Error(
+                    LogExceptionFilterAttributeFixtureController.NestedException.GetBaseException().Message,
+                    LogExceptionFilterAttributeFixtureController.NestedException.GetBaseException()));
+            }
+        }
+
+        private void ConfigureServer(InMemoryServer server)
+        {
+            server.Configuration.Filters.Add(new LogExceptionFilterAttribute(server.LoggerFactory));
+
+            server.Configuration.MapHttpAttributeRoutes();
+        }
+    }
+
+    [SuppressMessage("Microsoft.StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass",
+        Justification = "Embedding this controller class inside the fixture causes routing to its methods to fail.")]
+    [RoutePrefix("test")]
+    public class LogExceptionFilterAttributeFixtureController : ApiController
+    {
+        public static readonly InvalidOperationException Exception;
+
+        public static readonly InvalidOperationException NestedException;
+
+        [SuppressMessage("Microsoft.Design", "CA1065:DoNotRaiseExceptionsInUnexpectedLocations",
+            Justification = "This is simply for making testing raised exceptions easier.")]
+        static LogExceptionFilterAttributeFixtureController()
+        {
+            Exception = new InvalidOperationException("Error Message");
+            NestedException = new InvalidOperationException(
+                "Error Message", new InvalidOperationException("Inner Error Message"));
+        }
+
+        [Route("exception")]
+        public string GetException()
+        {
+            throw Exception;
+        }
+
+        [Route("nestedxception")]
+        public void GetNestedException()
+        {
+            throw NestedException;
+        }
+    }
+}
---
+++ src/Georadix.WebApi.Tests/SimpleInjectorWebApiDependencyResolverFixture.cs
@@ -0,0 +1,61 @@
+﻿namespace Georadix.WebApi.Tests
+{
+    using SimpleInjector;
+    using System;
+    using Xunit;
+
+    public class SimpleInjectorWebApiDependencyResolverFixture
+    {
+        [Fact]
+        public void CreateWithNullContainerThrowsArgumentNullException()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => new SimpleInjectorWebApiDependencyResolver(null));
+
+            Assert.Equal("container", ex.ParamName);
+
+            var sut = new SimpleInjectorWebApiDependencyResolver(new Container());
+        }
+
+        [Fact]
+        public void BeginScopeReturnsItself()
+        {
+            var sut = new SimpleInjectorWebApiDependencyResolver(new Container());
+
+            Assert.Same(sut, sut.BeginScope());
+        }
+
+        [Fact]
+        public void GetServiceGoesThroughTheContainerAndReturnTheService()
+        {
+            var container = new Container();
+
+            var sut = new SimpleInjectorWebApiDependencyResolver(container);
+
+            container.RegisterSingle(this.GetType(), this);
+
+            var service = sut.GetService(this.GetType());
+
+            Assert.Same(this, service);
+        }
+
+        [Fact]
+        public void GetServicesGoesThroughTheContainerAndReturnServices()
+        {
+            var container = new Container();
+
+            var sut = new SimpleInjectorWebApiDependencyResolver(container);
+
+            var services = sut.GetServices(this.GetType());
+
+            Assert.Empty(services);
+        }
+
+        [Fact]
+        public void DisposeDoesNothing()
+        {
+            var sut = new SimpleInjectorWebApiDependencyResolver(new Container());
+
+            sut.Dispose();
+        }
+    }
+}
