7a8fa45d97665751c847b22552e938d7f806214a
Stephane Alie <stephane@georadix.com>
2014-03-19 18:04:27 -0400

    0.0.0 is not a valid version apparently!

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -11,6 +11,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0")]
-[assembly: AssemblyInformationalVersion("0.0.0")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyInformationalVersion("0.0.1")]
