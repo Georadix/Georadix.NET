065ed331219861064a808139583b05ce5e0d983c
Stephane Alie <stephane@georadix.com>
2014-03-19 13:58:15 -0400

    Added CQRS pieces.

---
+++ src/Georadix.Data/IQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Georadix.Data
+{
+    /// <summary>
+    /// Defines a generic query handler.
+    /// </summary>
+    /// <typeparam name="TQuery">The type of query.</typeparam>
+    /// <typeparam name="TResult">The type of result.</typeparam>
+    public interface IQueryHandler<TQuery, TResult> where TQuery : IQuery<TResult>
+    {
+        /// <summary>
+        /// Handles the specified query.
+        /// </summary>
+        /// <param name="query">The query.</param>
+        /// <returns>The query's result.</returns>
+        TResult Handle(TQuery query);
+    }
+}
---
+++ src/Georadix.Services.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Georadix Services Tests Library")]
+[assembly: AssemblyDescription("Georadix Services Tests Library")]
+
+[assembly: Guid("929b5539-b8f5-46f3-805b-683bdce4f839")]
