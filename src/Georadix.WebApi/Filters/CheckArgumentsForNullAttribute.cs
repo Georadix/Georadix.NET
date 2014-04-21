4bbb63abdaaf8540fd2a6b817931b8c0341a6c9e
Anthony <anthony@georadix.com>
2014-04-21 16:40:35 -0400

    Added an action filter attribute to check for null arguments.

---
+++ src/Georadix.WebApi/Filters/CheckArgumentsForNullAttribute.cs
@@ -0,0 +1,52 @@
+﻿namespace Georadix.WebApi.Filters
+{
+    using Georadix.WebApi.Resources;
+    using System;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http.Controllers;
+    using System.Web.Http.Filters;
+    using System.Web.Http.ModelBinding;
+
+    /// <summary>
+    /// Represents a filter attribute used to check if the arguments are null.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, Inherited = true)]
+    public class CheckArgumentsForNullAttribute : ActionFilterAttribute
+    {
+        private readonly string[] arguments;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CheckArgumentsForNullAttribute" /> class with specified
+        /// arguments.
+        /// </summary>
+        /// <param name="arguments">
+        /// The arguments to check for <see langword="null"/>, or none to include all of them.
+        /// </param>
+        public CheckArgumentsForNullAttribute(params string[] arguments)
+        {
+            this.arguments = arguments;
+        }
+
+        /// <summary>
+        /// Occurs before the action method is invoked.
+        /// </summary>
+        /// <param name="actionContext">The action context.</param>
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            var args = (this.arguments == null) || (this.arguments.Length == 0) ?
+                actionContext.ActionArguments :
+                actionContext.ActionArguments.Where(arg => this.arguments.Contains(arg.Key));
+
+            args.Where(arg => arg.Value == null).ForEach(
+                arg => actionContext.ModelState.AddModelError(arg.Key, InvariantStrings.ParameterIsRequired));
+
+            if (!actionContext.ModelState.IsValid)
+            {
+                actionContext.Response = actionContext.Request.CreateErrorResponse(
+                    HttpStatusCode.BadRequest, actionContext.ModelState);
+            }
+        }
+    }
+}
