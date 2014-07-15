48075046dd668578e644a575e481480d152ab4c9
Anthony <anthony@georadix.com>
2014-07-15 17:51:41 -0400

    Script to install cert for testing.

---
+++ tools/InstallCert.ps1
@@ -0,0 +1,11 @@
+function Install-Certificate ($certPath, [string]$storeLocation = "LocalMachine", [string]$storeName = "TrustedPeople")
+{
+    $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($certPath, "", "MachineKeySet,PersistKeySet")
+    $store = New-Object System.Security.Cryptography.X509Certificates.X509Store($storeName, $storeLocation)
+    $store.Open("ReadWrite")
+    $store.Add($cert)
+    $store.Close()
+    "Thumbprint: $($cert.Thumbprint)"
+}
+
+Install-Certificate Dev.pfx
