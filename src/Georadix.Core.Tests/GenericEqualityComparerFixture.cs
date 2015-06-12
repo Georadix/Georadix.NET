10835ca0fd25ac72e8fe50b847e4427b784edd4e
Anthony <anthony@georadix.com>
2015-06-12 18:09:24 -0400

    Added a generic IEqualityComparer

---
+++ src/Georadix.Core.Tests/GenericEqualityComparerFixture.cs
@@ -0,0 +1,194 @@
+﻿namespace Georadix.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using Xunit;
+    using Xunit.Extensions;
+
+    public class GenericEqualityComparerFixture
+    {
+        private static DateTime sampleDateTime = DateTime.FromFileTimeUtc(1234567890);
+
+        private static Guid sampleGuid = Guid.NewGuid();
+
+        private static Model sampleModel = new Model(
+            DateTime.Now, Guid.NewGuid(), new Random().Next(), new Random().Next().ToString(), sampleSubModel);
+
+        private static SubModel sampleSubModel = new SubModel(
+            DateTime.Now, Guid.NewGuid(), new Random().Next(), new Random().Next().ToString());
+
+        public static IEnumerable<object[]> EqualsWithModelsScenarios
+        {
+            get
+            {
+                return new object[][]
+                {
+                    new object[]
+                    {
+                        sampleModel,
+                        sampleModel,
+                        true
+                    },
+                    new object[]
+                    {
+                        new Model(),
+                        new Model(),
+                        true
+                    },
+                    new object[]
+                    {
+                        new Model(
+                            new DateTime(2015, 06, 12),
+                            Guid.Empty,
+                            15,
+                            "A simple string",
+                            new SubModel(new DateTime(1234567890), Guid.Empty, -100, string.Empty)),
+                        new Model(
+                            new DateTime(2015, 06, 12),
+                            Guid.Empty,
+                            15,
+                            "A simple string",
+                            new SubModel(new DateTime(1234567890), Guid.Empty, -100, string.Empty)),
+                        true
+                    },
+                    new object[]
+                    {
+                        new Model() { GuidProp = Guid.NewGuid() },
+                        new Model(),
+                        false
+                    },
+                    new object[]
+                    {
+                        new Model() { SubModelProp = new SubModel() { DateTimeProp = DateTime.Now } },
+                        new Model() { SubModelProp = new SubModel() },
+                        false
+                    }
+                };
+            }
+        }
+
+        public static IEnumerable<object[]> EqualsWithSubModelsScenarios
+        {
+            get
+            {
+                return new object[][]
+                {
+                    new object[]
+                    {
+                        sampleSubModel,
+                        sampleSubModel,
+                        true
+                    },
+                    new object[]
+                    {
+                        new SubModel(),
+                        new SubModel(),
+                        true
+                    },
+                    new object[]
+                    {
+                        new SubModel(new DateTime(9876543210), Guid.Empty, 999, "SubModel string"),
+                        new SubModel(new DateTime(9876543210), Guid.Empty, 999, "SubModel string"),
+                        true
+                    },
+                    new object[]
+                    {
+                        new SubModel(),
+                        new SubModel() { DateTimeProp = DateTime.Now },
+                        false
+                    },
+                    new object[]
+                    {
+                        new SubModel() { IntProp = 10 },
+                        new SubModel() { IntProp = -10 },
+                        false
+                    }
+                };
+            }
+        }
+
+        [Theory]
+        [PropertyData("EqualsWithModelsScenarios")]
+        public void EqualsWithModelsReturnsExpectedResult(Model x, Model y, bool expected)
+        {
+            var sut = new GenericEqualityComparer<Model>();
+
+            Assert.Equal(expected, sut.Equals(x, y));
+        }
+
+        [Theory]
+        [PropertyData("EqualsWithSubModelsScenarios")]
+        public void EqualsWithSubModelsReturnsExpectedResult(SubModel x, SubModel y, bool expected)
+        {
+            var sut = new GenericEqualityComparer<SubModel>();
+
+            Assert.Equal(expected, sut.Equals(x, y));
+        }
+
+        [Fact]
+        public void GetHashCodeForNullObjectReturnsZero()
+        {
+            var sut = new GenericEqualityComparer<Model>();
+
+            Assert.Equal(0, sut.GetHashCode(null));
+        }
+
+        [Fact]
+        public void GetHashCodeForValidObjectReturnsObjectHashCode()
+        {
+            var sut = new GenericEqualityComparer<Model>();
+            var model = new Model();
+
+            Assert.Equal(model.GetHashCode(), sut.GetHashCode(model));
+        }
+
+        public class Model
+        {
+            public Model()
+            {
+            }
+
+            public Model(DateTime? dateTimeProp, Guid guidProp, int intProp, string stringProp, SubModel subModelProp)
+            {
+                this.DateTimeProp = dateTimeProp;
+                this.GuidProp = guidProp;
+                this.IntProp = intProp;
+                this.StringProp = stringProp;
+                this.SubModelProp = subModelProp;
+            }
+
+            public DateTime? DateTimeProp { get; private set; }
+
+            public Guid GuidProp { get; set; }
+
+            public int IntProp { get; private set; }
+
+            public string StringProp { get; set; }
+
+            public SubModel SubModelProp { get; set; }
+        }
+
+        public class SubModel
+        {
+            public SubModel()
+            {
+            }
+
+            public SubModel(DateTime? dateTimeProp, Guid guidProp, int intProp, string stringProp)
+            {
+                this.DateTimeProp = dateTimeProp;
+                this.GuidProp = guidProp;
+                this.IntProp = intProp;
+                this.StringProp = stringProp;
+            }
+
+            public DateTime? DateTimeProp { get; set; }
+
+            public Guid GuidProp { get; private set; }
+
+            public int IntProp { get; set; }
+
+            public string StringProp { get; private set; }
+        }
+    }
+}
---
+++ src/Georadix.Core/GenericEqualityComparer.cs
@@ -0,0 +1,79 @@
+﻿namespace Georadix.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+
+    /// <summary>
+    /// Provides an <see cref="IEqualityComparer{T}"/> implementation that recursively compares all the properties of
+    /// an object.
+    /// </summary>
+    /// <typeparam name="T">The type of object to compare.</typeparam>
+    public class GenericEqualityComparer<T> : IEqualityComparer<T>
+    {
+        private List<PropertyInfo> properties = new List<PropertyInfo>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenericEqualityComparer{T}"/> class.
+        /// </summary>
+        public GenericEqualityComparer()
+        {
+            this.properties.AddRange(
+                typeof(T).GetProperties(BindingFlags.GetProperty | BindingFlags.Instance | BindingFlags.Public));
+        }
+
+        /// <summary>
+        /// Determines whether the specified objects are equal.
+        /// </summary>
+        /// <param name="x">The first object to compare.</param>
+        /// <param name="y">The second object to compare.</param>
+        /// <returns>
+        /// <c>true</c> if the specified objects are equal; otherwise, <c>false</c>.
+        /// </returns>
+        public bool Equals(T x, T y)
+        {
+            foreach (var property in this.properties)
+            {
+                dynamic valueX = property.GetValue(x);
+                dynamic valueY = property.GetValue(y);
+
+                if ((valueX == null) || (valueY == null))
+                {
+                    if (valueX != valueY)
+                    {
+                        return false;
+                    }
+                }
+                else if (property.PropertyType.IsClass && (property.PropertyType != typeof(string)))
+                {
+                    var comparerType = typeof(GenericEqualityComparer<>).MakeGenericType(property.PropertyType);
+                    dynamic comparer = Activator.CreateInstance(comparerType);
+
+                    if (!comparer.Equals(valueX, valueY))
+                    {
+                        return false;
+                    }
+                }
+                else if (!valueX.Equals(valueY))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash
+        /// table.
+        /// </returns>
+        public int GetHashCode(T obj)
+        {
+            return (obj == null) ? 0 : obj.GetHashCode();
+        }
+    }
+}
