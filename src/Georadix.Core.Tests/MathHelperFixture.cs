28c37d1cfe48a7c45e7734426958cd7d836a44da
Anthony MacKinnon <anthony@georadix.com>
2018-01-09 13:34:12 -0500

    Great circle distance (#71)

--- src/Georadix.Core.Tests/MathHelperFixture.cs
+++ src/Georadix.Core.Tests/MathHelperFixture.cs
@@ -7,6 +7,15 @@
     public class MathHelperFixture
     {
         [Theory]
+        [InlineData(45, -75, 46, -76, 135786)]
+        [InlineData(-30, 50, 10, -20, 8692982)]
+        public void CalculateGreatCircleDistanceSucceeds(
+            double lat1, double long1, double lat2, double long2, double expected)
+        {
+            Assert.Equal(expected, MathHelper.CalculateGreatCircleDistance(lat1, long1, lat2, long2), 0);
+        }
+
+        [Theory]
         [InlineData(-180, -Math.PI)]
         [InlineData(-90, -Math.PI / 2)]
         [InlineData(0, 0)]
